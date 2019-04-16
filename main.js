b936d16250b685d1314c514291629981ab92a0ba
Dorotka <dorotamamczak@gmail.com>
2019-04-16 22:50:31 +0200

    created slider - cars

---
+++ main.js
@@ -0,0 +1,47 @@
+const carLeft = document.querySelector('.car.left');
+const carActive = document.querySelector('.car.active');
+const carRight = document.querySelector('.car.right');
+const arrowLeft = document.querySelector('.arrow.left');
+const arrowRight = document.querySelector('.arrow.right');
+const cars = ['car0', 'car1', 'car2'];
+let indexLeft = 0;
+let indexActive = 1;
+let indexRight = 2;
+
+const changeCarLeftDirection = () => {
+    indexLeft++;
+    indexActive++;
+    indexRight++;
+
+    console.log(indexLeft, indexActive, indexRight)
+
+    indexLeft < cars.length ? carLeft.style.backgroundImage = `url(img/${cars[indexLeft]}.png)` : carLeft.style.backgroundImage = `url(img/${cars[indexLeft = 0]}.png)`;
+
+    indexActive < cars.length ? carActive.style.backgroundImage = `url(img/${cars[indexActive]}.png)` : carActive.style.backgroundImage = `url(img/${cars[indexActive = 0]}.png)`;
+
+    indexRight < cars.length ? carRight.style.backgroundImage = `url(img/${cars[indexRight]}.png)` : carRight.style.backgroundImage = `url(img/${cars[indexRight = 0]}.png)`;
+}
+
+const changeCarRightDirection = () => {
+    indexLeft--;
+    indexActive--;
+    indexRight--;
+
+    console.log(indexLeft, indexActive, indexRight)
+
+    indexLeft >= 0 ? carLeft.style.backgroundImage = `url(img/${cars[indexLeft]}.png)` : carLeft.style.backgroundImage = `url(img/${cars[indexLeft = cars.length - 1]}.png)`;
+
+    indexActive >= 0 ? carActive.style.backgroundImage = `url(img/${cars[indexActive]}.png)` : carActive.style.backgroundImage = `url(img/${cars[indexActive = cars.length - 1]}.png)`;
+
+    indexRight >= 0 ? carRight.style.backgroundImage = `url(img/${cars[indexRight]}.png)` : carRight.style.backgroundImage = `url(img/${cars[indexRight = cars.length - 1]}.png)`;
+}
+
+arrowLeft.addEventListener('click', changeCarLeftDirection);
+arrowRight.addEventListener('click', changeCarRightDirection);
+carLeft.addEventListener('click', changeCarLeftDirection);
+carRight.addEventListener('click', changeCarRightDirection);
+
+// ---- TODO:
+// create carActive.animate
+// add cars.json
+// make this page responsive
