c27d7d9f817ebbf1734486868467210f38c21c49
Dorotka <dorotamamczak@gmail.com>
2019-04-17 12:13:11 +0200

    add animate

--- main.js
+++ main.js
@@ -9,6 +9,19 @@ let indexActive = 1;
 let indexRight = 2;
 
 const changeCarLeftDirection = () => {
+    // carActive.animate([{
+    //         // filter: 'brightness(0)',
+    //         transform: 'scale(0.9) translate(-50%, -50%)',
+    //     },
+    //     {
+    //         // filter: 'brightness(1)',
+    //         transform: 'scale(1) translate(-50%, -50%)',
+    //     }
+    // ], {
+    //     duration: 500,
+    //     fill: "both",
+    // });
+
     indexLeft++;
     indexActive++;
     indexRight++;
@@ -20,9 +33,24 @@ const changeCarLeftDirection = () => {
     indexActive < cars.length ? carActive.style.backgroundImage = `url(img/${cars[indexActive]}.png)` : carActive.style.backgroundImage = `url(img/${cars[indexActive = 0]}.png)`;
 
     indexRight < cars.length ? carRight.style.backgroundImage = `url(img/${cars[indexRight]}.png)` : carRight.style.backgroundImage = `url(img/${cars[indexRight = 0]}.png)`;
+
+
 }
 
 const changeCarRightDirection = () => {
+    // carActive.animate([{
+    //         // filter: 'brightness(0)',
+    //         transform: 'scale(0.9) translate(-50%, -50%)',
+    //     },
+    //     {
+    //         // filter: 'brightness(1)',
+    //         transform: 'scale(1) translate(-50%, -50%)',
+    //     }
+    // ], {
+    //     duration: 500,
+    //     fill: "both",
+    // });
+
     indexLeft--;
     indexActive--;
     indexRight--;
