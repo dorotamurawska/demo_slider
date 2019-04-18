d86d0461e8bc0358b9fc5e6d0b89a8321c3409b5
Dorotka <dorotamamczak@gmail.com>
2019-04-18 17:25:58 +0200

    add new json file

--- main.js
+++ main.js
@@ -1,75 +1,159 @@
+let carsObj = {}
+const arrayCars = [];
+
+const sliderSection = document.querySelector('section.slider');
+const requestURL = 'cars.json';
+const request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'text';
+request.send();
+
+request.onload = function () {
+    const carsText = request.response;
+    carsObj = JSON.parse(carsText);
+    createArray(carsObj);
+    // changeCarLeftDirection(carsObj);
+}
+
+// fetch('http://example.com/movies.json')
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (myJson) {
+//         console.log(JSON.stringify(myJson));
+//     });
+
+
+const carsPath = ['car0', 'car1', 'car2'];
+let indexLeft = 0;
+let indexActive = 1;
+let indexRight = 2;
+
+
+const createArray = (jsonObj) => {
+    const numCars = Object.keys(jsonObj).length;
+
+    // --- SLIDER - create elements in html ---
+
+    for (let i = 0; i < numCars; i++) {
+
+        const divCar = document.createElement('div');
+        divCar.classList.add('car');
+        divCar.style.backgroundImage = `url(img/${jsonObj[`car${i}`].path})`;
+
+        const h1Car = document.createElement('h1');
+        h1Car.textContent = `${jsonObj[`car${i}`].name}`;
+
+        const pCar = document.createElement('p');
+        pCar.textContent = `${jsonObj[`car${i}`].desc}`;
+
+        // --- SLIDER - start position ---
+        if (i === indexLeft) {
+            divCar.classList.add('left');
+        }
+        if (i === indexActive) {
+            divCar.classList.add('active');
+            h1Car.classList.add('active');
+            pCar.classList.add('active');
+        }
+        if (i === indexRight) {
+            divCar.classList.add('right');
+        }
+
+        sliderSection.appendChild(divCar);
+        sliderSection.appendChild(h1Car);
+        sliderSection.appendChild(pCar);
+    }
+}
+
+const changeSlide = () => {
+
+    console.log(document.querySelector('.active'));
+
+}
+
+changeSlide()
+
+// console.log(arrayCars)
+
 const carLeft = document.querySelector('.car.left');
 const carActive = document.querySelector('.car.active');
 const carRight = document.querySelector('.car.right');
 const arrowLeft = document.querySelector('.arrow.left');
 const arrowRight = document.querySelector('.arrow.right');
-const cars = ['car0', 'car1', 'car2'];
-let indexLeft = 0;
-let indexActive = 1;
-let indexRight = 2;
-
-const changeCarLeftDirection = () => {
-    // carActive.animate([{
-    //         // filter: 'brightness(0)',
-    //         transform: 'scale(0.9) translate(-50%, -50%)',
-    //     },
-    //     {
-    //         // filter: 'brightness(1)',
-    //         transform: 'scale(1) translate(-50%, -50%)',
-    //     }
-    // ], {
-    //     duration: 500,
-    //     fill: "both",
-    // });
-
-    indexLeft++;
-    indexActive++;
-    indexRight++;
-
-    console.log(indexLeft, indexActive, indexRight)
-
-    indexLeft < cars.length ? carLeft.style.backgroundImage = `url(img/${cars[indexLeft]}.png)` : carLeft.style.backgroundImage = `url(img/${cars[indexLeft = 0]}.png)`;
-
-    indexActive < cars.length ? carActive.style.backgroundImage = `url(img/${cars[indexActive]}.png)` : carActive.style.backgroundImage = `url(img/${cars[indexActive = 0]}.png)`;
-
-    indexRight < cars.length ? carRight.style.backgroundImage = `url(img/${cars[indexRight]}.png)` : carRight.style.backgroundImage = `url(img/${cars[indexRight = 0]}.png)`;
 
 
+const setAnimate = () => {
+
+    carActive.animate([{
+            filter: 'opacity(0.4)',
+            transform: 'scale(1.1) translate(-50%, -50%)',
+            transformOrigin: 'left',
+        },
+        {
+            filter: 'opacity(1)',
+            transform: 'scale(1) translate(-50%, -50%)',
+            transformOrigin: 'left'
+        }
+    ], {
+        duration: 700,
+        fill: "both",
+    });
 }
 
-const changeCarRightDirection = () => {
-    // carActive.animate([{
-    //         // filter: 'brightness(0)',
-    //         transform: 'scale(0.9) translate(-50%, -50%)',
-    //     },
-    //     {
-    //         // filter: 'brightness(1)',
-    //         transform: 'scale(1) translate(-50%, -50%)',
-    //     }
-    // ], {
-    //     duration: 500,
-    //     fill: "both",
-    // });
 
-    indexLeft--;
-    indexActive--;
-    indexRight--;
 
-    console.log(indexLeft, indexActive, indexRight)
+// const changeCarLeftDirection = (jsonObj) => {
+//     const numCars = Object.keys(jsonObj).length;
 
-    indexLeft >= 0 ? carLeft.style.backgroundImage = `url(img/${cars[indexLeft]}.png)` : carLeft.style.backgroundImage = `url(img/${cars[indexLeft = cars.length - 1]}.png)`;
 
-    indexActive >= 0 ? carActive.style.backgroundImage = `url(img/${cars[indexActive]}.png)` : carActive.style.backgroundImage = `url(img/${cars[indexActive = cars.length - 1]}.png)`;
+//     console.log(numCars)
 
-    indexRight >= 0 ? carRight.style.backgroundImage = `url(img/${cars[indexRight]}.png)` : carRight.style.backgroundImage = `url(img/${cars[indexRight = cars.length - 1]}.png)`;
-}
+//     setAnimate();
 
-arrowLeft.addEventListener('click', changeCarLeftDirection);
-arrowRight.addEventListener('click', changeCarRightDirection);
-carLeft.addEventListener('click', changeCarLeftDirection);
-carRight.addEventListener('click', changeCarRightDirection);
+//     console.log(indexLeft, indexActive, indexRight)
+
+//     indexRight < numCars ? carRight.style.backgroundImage = `url(img/${jsonObj[`car${indexRight}`].path})` : carRight.style.backgroundImage = `url(img/${jsonObj[`car${indexRight = 0}`].path})`;
+
+//     console.log(`url(img/${jsonObj[`car${indexLeft}`].path})`)
+
+//     indexActive < numCars ? carActive.style.backgroundImage = `url(img/${jsonObj[`car${indexActive}`].path})` : carActive.style.backgroundImage = `url(img/${jsonObj[`car${indexActive = 0}`].path})`;
+
+//     console.log(`url(img/${jsonObj[`car${indexActive}`].path})`)
+
+//     indexRight < numCars ? carRight.style.backgroundImage = `url(img/${jsonObj[`car${indexRight}`].path})` : carRight.style.backgroundImage = `url(img/${jsonObj[`car${indexRight = 0}`].path})`;
+
+//     console.log(`url(img/${jsonObj[`car${indexRight}`].path})`)
+
+
+//     console.log(numCars)
+//     indexLeft++;
+//     indexActive++;
+//     indexRight++;
+// }
+
+// const changeCarRightDirection = () => {
+//     setAnimate();
+
+//     indexLeft--;
+//     indexActive--;
+//     indexRight--;
+
+//     console.log(indexLeft, indexActive, indexRight)
+
+//     indexLeft >= 0 ? carLeft.style.backgroundImage = `url(img/${carsPath[indexLeft]}.png)` : carLeft.style.backgroundImage = `url(img/${carsPath[indexLeft = carsPath.length - 1]}.png)`;
+
+//     indexActive >= 0 ? carActive.style.backgroundImage = `url(img/${carsPath[indexActive]}.png)` : carActive.style.backgroundImage = `url(img/${carsPath[indexActive = carsPath.length - 1]}.png)`;
+
+//     indexRight >= 0 ? carRight.style.backgroundImage = `url(img/${carsPath[indexRight]}.png)` : carRight.style.backgroundImage = `url(img/${carsPath[indexRight = carsPath.length - 1]}.png)`;
+// }
+
+// arrowLeft.addEventListener('click', changeCarLeftDirection);
+// arrowRight.addEventListener('click', changeCarRightDirection);
+// carLeft.addEventListener('click', changeCarLeftDirection);
+// carRight.addEventListener('click', changeCarRightDirection);
+
 
 // ---- TODO:
-// create carActive.animate
 // add cars.json
 // make this page responsive
